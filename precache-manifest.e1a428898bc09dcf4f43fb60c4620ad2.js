12a8d450b342772f2b8b03c7b8d54cb2a98643d1
unknown <rpdud10@naver.com>
2020-03-10 16:02:51 +0900

    Updates

---
+++ precache-manifest.e1a428898bc09dcf4f43fb60c4620ad2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fd9a0f61a252af54cd0fa466d538e10",
+    "url": "/moviejgy_app_2020/index.html"
+  },
+  {
+    "revision": "b23b42338161f49a574e",
+    "url": "/moviejgy_app_2020/static/css/main.c3b3b69d.chunk.css"
+  },
+  {
+    "revision": "9e97dd5826ec1cfa467a",
+    "url": "/moviejgy_app_2020/static/js/2.688fded6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/moviejgy_app_2020/static/js/2.688fded6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b23b42338161f49a574e",
+    "url": "/moviejgy_app_2020/static/js/main.79a35e81.chunk.js"
+  },
+  {
+    "revision": "468820f292aaebb1da5f",
+    "url": "/moviejgy_app_2020/static/js/runtime-main.8c094ec7.js"
+  }
+]);
